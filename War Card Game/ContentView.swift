a557b526a15598b82bce89e01eaf2c6404ef2236
Brian Simmons <bsimmons01@gmail.com>
2025-03-13 15:31:06 -0700

    Initial Commit

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,13 +9,131 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Image("card2")
+                    
+                    Spacer()
+                    
+                    Image("card3")
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Image("button")
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                
+                Spacer()
+                
+            }
         }
-        .padding()
+        
+        
+        
+        
+        // My attempt
+//        ZStack {
+//            Color(red: 67/255, green: 134/255, blue: 91/255)
+//            
+//            VStack {
+//                Spacer()
+//                
+//                Image("logo")
+//                
+//                Spacer()
+//                
+//                HStack {
+//                    Image("card2")
+//                    
+//                    Spacer()
+//                    
+//                    Image("card3")
+//                }
+//                .padding(.horizontal, 50)
+//
+//                Spacer()
+//                
+//                Image("button")
+//                
+//                Spacer()
+//                
+//                HStack {
+//                    VStack {
+//                        Text("Player")
+//                            .minimumScaleFactor(0.5)
+//                            .lineLimit(1)
+//                            .font(.title3)
+//                            .padding(.bottom, 10)
+//                        
+//                        Text("0")
+//                            .font(.title)
+//                    }
+//                    
+//                    Spacer()
+//                    
+//                    VStack {
+//                        Text("CPU")
+//                            .minimumScaleFactor(0.5)
+//                            .lineLimit(1)
+//                            .font(.title3)
+//                            .padding(.bottom, 10)
+//                        
+//                        Text("0")
+//                            .font(.title)
+//                    }
+//                }
+//                .padding(.horizontal, 100)
+//                .foregroundColor(.white)
+//                
+//                Spacer()
+//                
+//            }
+//        }
+//        .ignoresSafeArea()
+        
     }
 }
 
