c0db3e2f9136a04c280ac4d7da38b6b5b4acb329
Brian Simmons <bsimmons01@gmail.com>
2025-03-13 14:17:18 -0700

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Brian Simmons on 3/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
